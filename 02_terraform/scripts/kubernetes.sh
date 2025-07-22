631326fe08107668e17610ed8503b7329c9fd1e6
BelaouraHamza <belaoura.hamza@yahoo.com>
2025-07-22 14:53:40 +0200

    feat: ajout des manifestes K8s pour le déploiement de Odoo, PostgreSQL et PGAdmin

--- 02_terraform/scripts/kubernetes.sh
+++ 02_terraform/scripts/kubernetes.sh
@@ -6,7 +6,7 @@ export host_ip=$(curl -s ifconfig.co/ip)
 # Update the package list
 sudo apt-get update -y
 sudo apt install curl -y
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable=traefik --tls-sans=$host_ip" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable=traefik --tls-san=$host_ip" sh -
 
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
